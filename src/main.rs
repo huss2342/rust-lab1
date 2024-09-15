ee9683ad09df2b332e2e036749d8e510b6145370
Hussein <a.hussein@wustl.edu>
2024-09-15 13:30:15 -0500

    finished question 4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
+use std::env;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+static WHINGE_MODE: AtomicBool = AtomicBool::new(false);
+
 fn main() {
     println!("Hello, world!");
 
+}
+
+fn parse_args(config_file_name: &mut String) -> Result<(), u8> {
+    let mut args: Vec<String> = Vec::new();
+
+    for arg in env::args() {
+        args.push(arg);
+    }
+
+    if args.len() < 2 || args.len() > 3 || (args.len() == 3 && args[2] != "whinge") {
+        usage(&args[0]);
+        return Err(1);
+    }
+
+    *config_file_name = args[1].clone();
+    WHINGE_MODE.store(true, Ordering::SeqCst);
+
+    Ok(())
 }
 
 fn usage(program_name: &String) {
